a2249e536c2f2ec71c1933024cbf29fb632ed0c2
KeshabNeupane <keshabneupane061@gmail.com>
2025-06-30 13:15:56 +0545

    addtask functionality added

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    
+    path('/addtask/',views.AddTask,name='AddTask'),
+    path('/updatetask/',views.updatetask,name='updatetask'),
+    path('/completedtask/',views.completedtask,name='completedtaks'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Task
 # Create your views here.
+
+def AddTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
+
+def completedtask(request):
+    completedtask = Task.objects.filter(is_completed=True)
+    context = {
+        'completedtask':completedtask
+    }
+    return render(request,'home.html',context)
+
+def updatetask(request):
+    # is_completed = Task.object.get(task='')
+    return HttpResponse('from update views')
