c711f1016a565e21d330b531ed00c2262534f597
KeshabNeupane <keshabneupane061@gmail.com>
2025-06-30 09:04:02 +0545

    admin pannel costumize

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Task
-
+ 
+#admin pannel costumize
 class Taskadmin(admin.ModelAdmin):
     list_display = ('task','is_completed','updated_at')
 
