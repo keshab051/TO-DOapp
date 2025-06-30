81656a64634075a9ab51ddc6b6c6ed122fe68535
KeshabNeupane <keshabneupane061@gmail.com>
2025-06-30 09:01:18 +0545

    model task is made as Task

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import task
+from .models import Task
+
+class Taskadmin(admin.ModelAdmin):
+    list_display = ('task','is_completed','updated_at')
 
 # Register your models here.
-admin.site.register(task)
+admin.site.register(Task,Taskadmin)
--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class task(models.Model):
+class Task(models.Model):
 
     task = models.CharField(max_length=250)
     is_completed = models.BooleanField(default=False)
