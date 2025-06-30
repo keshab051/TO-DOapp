eb694bfce61ce64871500291a8fa38d6812203fe
KeshabNeupane <keshabneupane061@gmail.com>
2025-06-30 13:59:48 +0545

    mark_as_done and undo button functionality added

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('/addtask/',views.AddTask,name='AddTask'),
-    path('/updatetask/',views.updatetask,name='updatetask'),
-    path('/completedtask/',views.completedtask,name='completedtaks'),
+    path('/mark_as_done/<int:pk>',views.mark_as_done,name='mark_as_done'),
+    path('/mark_as_undo/<int:pk>',views.mark_as_undo,name='mark_as_undo'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Task
 # Create your views here.
@@ -8,13 +8,15 @@ def AddTask(request):
     Task.objects.create(task=task)
     return redirect('home')
 
-def completedtask(request):
-    completedtask = Task.objects.filter(is_completed=True)
-    context = {
-        'completedtask':completedtask
-    }
-    return render(request,'home.html',context)
+def mark_as_done(request,pk):
+    completed_task = get_object_or_404(Task,pk=pk)
+    completed_task.is_completed = True
+    completed_task.save()
+    return redirect('home')
 
-def updatetask(request):
-    # is_completed = Task.object.get(task='')
-    return HttpResponse('from update views')
+def mark_as_undo(request,pk):
+    undo_task = get_object_or_404(Task,pk=pk)
+    undo_task.is_completed = False
+    undo_task.save()
+    return redirect('home')
+    # return HttpResponse('from mark_as_undo')
